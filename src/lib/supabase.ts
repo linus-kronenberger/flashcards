37b8c7ea6bf31943484a47d462a4c1d7e0efa3d5
I749983 <lkronenb1@googlemail.com>
2025-06-03 00:09:09 +0200

    update

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,7 +3,7 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!
+const supabaseKey = process.env.AWS_SECRET_ACCESS_KEY!
 
 export async function getServerClient() : Promise<SupabaseClient>{
     const supabase = createClient(supabaseUrl, supabaseKey);
