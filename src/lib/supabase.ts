fb2a826d30f079d4c94ccaa07e926d4c7c9fe73f
I749983 <lkronenb1@googlemail.com>
2025-06-03 00:08:57 +0200

    using right api key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,23 +3,7 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
-export async function testDatabaseConnection() : Promise<boolean> {
-    console.log('🔄 Teste Supabase Datenbankverbindung...');
-    try {
-        console.log('🔗 Supabase URL:', supabaseUrl);
-        getServerClient().then(() => {
-            console.log('✅ Supabase Client erfolgreich erstellt!');
-        });
-       
-    } catch (error) { 
-        console.error('❌ Fehler beim Erstellen des Supabase Clients:', error);
-        return false;
-    }
-
-    return true;
-}
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!
 
 export async function getServerClient() : Promise<SupabaseClient>{
     const supabase = createClient(supabaseUrl, supabaseKey);
